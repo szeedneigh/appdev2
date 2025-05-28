771a429f53b84fd52a428afae39b784379a6ff54
szeedneigh <sidneyjohn1704@gmail.com>
2025-05-29 01:24:13 +0800

    feat: add book model

---
+++ 10_simple_book_api_with_authentication/models/book.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Book title is required'],
+    trim: true
+  },
+  author: {
+    type: String,
+    required: [true, 'Author name is required'],
+    trim: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Book', bookSchema);
